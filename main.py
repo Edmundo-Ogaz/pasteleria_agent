1d55bfc627d152b51e3a3d8b9e01611263adda55
Edmundo Ogaz <edmundo.ogaz@gmail.com>
2025-03-06 13:25:05 -0300

    human in the loop

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from typing import Annotated
 
 from typing_extensions import TypedDict
 
+from langchain_core.tools import tool
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.checkpoint.memory import MemorySaver
+from langgraph.types import Command, interrupt
 from langchain_groq import ChatGroq
 
 class State(TypedDict):
@@ -17,6 +19,12 @@ class State(TypedDict):
 
 graph_builder = StateGraph(State)
 
+@tool
+def human_assistance(query: str) -> str:
+    """Request assistance from a human."""
+    human_response = interrupt({"query": query})
+    return human_response["data"]
+
 def get_info_about_the_bakery() -> str:
     """function to get info about the bakery for example: history, address, fone"""
     return "pastelería la palmera"
@@ -25,13 +33,18 @@ def get_products_by_ingredient(ingredient: str) -> str:
     """function to get the products by a ingredient"""
     return "piña"
 
-tools = [get_info_about_the_bakery, get_products_by_ingredient]
+tools = [get_info_about_the_bakery, get_products_by_ingredient, human_assistance]
 
 llm = ChatGroq( model="llama-3.3-70b-versatile", temperature=0.0, max_retries=2)
 llm_with_tools = llm.bind_tools(tools)
 
 def chatbot(state: State):
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+    message = llm_with_tools.invoke(state["messages"])
+    # Because we will be interrupting during tool execution,
+    # we disable parallel tool calling to avoid repeating any
+    # tool invocations when we resume.
+    assert len(message.tool_calls) <= 1
+    return {"messages": [message]}
 
 
 # The first argument is the unique node name
@@ -39,7 +52,7 @@ def chatbot(state: State):
 # the node is used.
 graph_builder.add_node("chatbot", chatbot)
 
-graph_builder.add_node("tools", ToolNode(tools=[get_info_about_the_bakery, get_products_by_ingredient]))
+graph_builder.add_node("tools", ToolNode(tools=tools))
 
 graph_builder.add_conditional_edges(
     "chatbot",
@@ -58,35 +71,52 @@ memory = MemorySaver()
 
 graph = graph_builder.compile(checkpointer=memory)
 
-def stream_graph_updates(user_input: str):
-    config = {"configurable": {"thread_id": "1"}}
-    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}, config):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
+# def stream_graph_updates(user_input: str):
+#     config = {"configurable": {"thread_id": "1"}}
+#     for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}, config):
+#         for value in event.values():
+#             print("Assistant:", value["messages"][-1].content)
 
-    snapshot = graph.get_state(config)
-    print(snapshot)
+#     snapshot = graph.get_state(config)
+#     print(snapshot)
 
-    # events = graph.stream(
-    #     {"messages": [{"role": "user", "content": user_input}]},
-    #     {"configurable": {"thread_id": "1"}},
-    #     stream_mode="values",
-    # )
-    # for event in events:
-    #     event["messages"][-1].pretty_print()
+# while True:
+#     try:
+#         user_input = input("User: ")
+#         if user_input.lower() in ["quit", "exit", "q"]:
+#             print("Goodbye!")
+#             break
 
+#         stream_graph_updates(user_input)
+#     except:
+#         # fallback if input() is not available
+#         user_input = "What do you know about LangGraph?"
+#         print("User: " + user_input)
+#         stream_graph_updates(user_input)
+#         break
 
-while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
-            break
+user_input = "I need someone expert guidance. Could you request assistance for me?"
+config = {"configurable": {"thread_id": "1"}}
 
-        stream_graph_updates(user_input)
-    except:
-        # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
-        break
+events = graph.stream(
+    {"messages": [{"role": "user", "content": user_input}]},
+    config,
+    stream_mode="values",
+)
+for event in events:
+    if "messages" in event:
+        event["messages"][-1].pretty_print()
+
+snapshot = graph.get_state(config)
+print(snapshot.next)
+
+human_response = (
+    "We, the experts are here to help!"
+)
+
+human_command = Command(resume={"data": human_response})
+
+events = graph.stream(human_command, config, stream_mode="values")
+for event in events:
+    if "messages" in event:
+        event["messages"][-1].pretty_print()
